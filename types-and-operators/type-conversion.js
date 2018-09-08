69142349280aa85c92bd7e64e9e5a250277f9a20
brian.gormanly <bgormanly@gmail.com>
2018-09-08 18:08:30 -0400

    added type-conversion to datatypes

---
+++ types-and-operators/type-conversion.js
@@ -0,0 +1,15 @@
+// do 1 at a time in the browser console
+8 * null;
+
+"5" - 1;
+
+"5" + 1;
+
+"five" * 2;
+
+"5" * 2;
+
+false == 0;
+
+// when discussing type conversion and when it happens == vs ===
+false === 0;
