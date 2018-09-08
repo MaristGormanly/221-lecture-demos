6a8a66eb0987776cace93c0db29fb32b5672e9dd
brian.gormanly <bgormanly@gmail.com>
2018-09-08 16:46:41 -0400

    added imperative-declarative demo to intro to js

---
+++ intro-to-js/imperative-declarative.js
@@ -0,0 +1,32 @@
+var numbers = [1,2,3,4,5];
+
+var doubled = [];
+
+// Imperative: Create a value in doubled array for each in numbers 
+// that contains the doubled value of the corrisponding value in
+// numbers
+for(var i=0; i<numbers.length; i++) {
+	doubled.push(numbers[i] * 2);
+}
+console.log("numbers: " + numbers);
+console.log("doubled: " + doubled);
+
+
+
+
+var numbers = [2,3,4,5,6];
+
+var doubled = [];
+
+// Declarative: accomplishes the same thing as the imperative code
+// 
+
+//var doubled = numbers.map((n) => n*2); // simplified version
+var doubled = numbers.map(function(n){return n*2});  // here is a more complete version
+
+console.log("numbers: " + numbers);
+console.log("doubled: " + doubled);
+
+
+// here is a more complete version 
+var doubled = numbers.map(function(n){return n*2});
