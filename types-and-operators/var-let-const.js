02c25fcb3af023b1fbce4ddf06567d7dc46ed8cd
brian.gormanly <bgormanly@gmail.com>
2018-09-08 17:52:04 -0400

    added var-let-const demo to types and operators

---
+++ types-and-operators/var-let-const.js
@@ -0,0 +1,50 @@
+// shows the difference between var and no declared scope (global property)
+var testVar = "orginal testVar";
+testNothing = "orginal testNothing";
+console.log("1: " + testVar);
+console.log("1: " + testNothing);
+
+function scopeTester() {
+	var testVar = "changed testVar";
+	testNothing = "changed testNothing";
+
+	console.log("2: " + testVar);
+	console.log("2: " + testNothing);
+}
+
+scopeTester();
+
+console.log("3: " + testVar);
+console.log("3: " + testNothing);
+
+
+
+
+// shows the difference between var and let (let has block level scope)
+console.log("This is level 1");
+
+if(true) {
+	console.log("True was true! Bet you did not see that coming!");
+	var testVar = "This is a var";
+	let testLet = "This is a let";
+}
+
+console.log("var test:" + testVar);
+console.log("let test:" + testLet);
+
+
+
+
+
+// shows the difference between let and const
+let testLet = "original let";
+const TEST_CONST = "original const";
+
+console.log("1: " + testLet);
+console.log("1: " + TEST_CONST);
+
+testLet = "changed let";
+TEST_CONST = "changed const";
+
+console.log("2: " + testLet);
+console.log("2: " + TEST_CONST);
