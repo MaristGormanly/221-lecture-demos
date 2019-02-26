ea11bc08f51c2172ad86524fe4dd7bcb63cbb2a5
brian.gormanly <bgormanly@gmail.com>
2019-02-25 19:41:07 -0500

    updates to events

---
+++ events/counting.js
@@ -0,0 +1,9 @@
+
+
+comments.addEventListener('keyup', function(e) {
+    var count = document.getElementById('comments').value.length;
+    document.getElementById('count').value = count;
+    if (count > 100) {
+        comments.value = comments.value.slice(0,100);
+    }
+});
--- intro-to-js/imperative-declarative.js
+++ intro-to-js/imperative-declarative.js
@@ -1,8 +1,7 @@
 var numbers = [1,2,3,4,5];
-
 var doubled = [];
 
-// Imperative: Create a value in doubled array for each in numbers 
+// Imperative: Create a value in doubled array for each in numbers
 // that contains the doubled value of the corrisponding value in
 // numbers
 for(var i=0; i<numbers.length; i++) {
@@ -15,18 +14,16 @@ console.log("doubled: " + doubled);
 
 
 var numbers = [2,3,4,5,6];
-
 var doubled = [];
 
 // Declarative: accomplishes the same thing as the imperative code
-// 
-
-//var doubled = numbers.map((n) => n*2); // simplified version
 var doubled = numbers.map(function(n){return n*2});  // here is a more complete version
+
+//var doubled = numbers.map(n => n * 2); // simple version
 
 console.log("numbers: " + numbers);
 console.log("doubled: " + doubled);
 
 
-// here is a more complete version 
+// here is a more complete version
 var doubled = numbers.map(function(n){return n*2});
