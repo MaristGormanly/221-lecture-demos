dd871e9008d78477578b7514716940869a9fe16a
Brian Gormanly <bgormanly@gmail.com>
2020-01-18 12:13:42 -0500

    playing around with promise demo

--- Promises/promises.js
+++ Promises/promises.js
@@ -5,21 +5,25 @@ function myFunction(price) {
 
 		setTimeout(function() {
 			resolve(price + 1);
-		}, 300);
+		}, 1000);
 	});
 }
 
-myFunction(1)
+/*
+myFunction(5)
 .then(function(newPrice) {
 	console.log(newPrice);
 }).catch(function(error) {
-	console.log(error);
+	console.log("my reject " + error);
 })
 
 
 
-/*
+*/
 // static Promise
+
+
+
 function createPrice(price, delay) {
 	return new Promise(function(resolve, reject) {
 		if(price > 100) reject("This failed!");
@@ -30,18 +34,25 @@ function createPrice(price, delay) {
 	});
 }
 
-//createPrice(5, 1000).then(function(rPrice) {console.log(rPrice)});
-*/
+let promises = [
+	createPrice(3, 1800).then((resolvedPrice) => resolvedPrice).catch((error) => console.log(error)),
+	createPrice(90, 1100).then((resolvedPrice) => resolvedPrice).catch((error) => console.log(error))
+];
 
-// without chaining, has nested resolve and reject handling
-/*
-var promises = [createPrice(3, 1100).then((resolvedPrice) => resolvedPrice), createPrice(5, 1600).then((resolvedPrice) => resolvedPrice)];
 Promise.all(promises).then(function(resolvedPromises) {
 	console.log(resolvedPromises);
 })
+
+
+
+/*
+createPrice(5, 1000).then(function(rPrice) {console.log(rPrice * 100)});
 */
 
+// without chaining, has nested resolve and reject handling
+
 // now with chaining (resolve and reject handled at the bottom of the chain)
+
 /*
 var promises = [createPrice(3, 1100), createPrice(105, 1300)];
 
